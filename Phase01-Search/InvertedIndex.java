1afb6a3eacd75c4581ac2f955d14fabe961c7aef
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2020-08-01 17:27:56 +0430

    Added InvertedIndex Class

---
+++ Phase01-Search/InvertedIndex.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class InvertedIndex {
+    public HashMap<String, ArrayList<Integer>> dictMap;
+
+    public InvertedIndex() {
+        dictMap = new HashMap<>();
+    }
+
+    public void makeChanges(String newDocument, int documentID) {
+        String fullToken = newDocument.toLowerCase();
+        String[] tokens = fullToken.split(" ");
+        for (String str : tokens) {
+            if (!dictMap.containsKey(str)) {
+                dictMap.put(str, new ArrayList<>());
+            }
+            dictMap.get(str).add(documentID);
+        }
+    }
+}
