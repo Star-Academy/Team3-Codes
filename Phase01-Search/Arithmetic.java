9344162efff6e1fbb16b01052688b8b56e91d09f
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2020-08-02 12:13:53 +0430

    arithmetic is complete now

--- Phase01-Search/Arithmetic.java
+++ Phase01-Search/Arithmetic.java
@@ -1,16 +1,26 @@
 import java.util.ArrayList;
 
 public class Arithmetic {
-    
+
     public ArrayList<Integer> and(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
-        return null;
+        ArrayList<Integer> temp = new ArrayList<Integer>(arr1);
+        temp.retainAll(arr2);
+        return temp;
     }
 
     public ArrayList<Integer> or(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
-        return null;
+        ArrayList<Integer> temp = new ArrayList<Integer>(arr1);
+        for (Integer integer : arr2) {
+            if (!temp.contains(integer)) {
+                temp.add(integer);
+            }
+        }
+        return temp;
     }
 
     public ArrayList<Integer> subtract(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
-        return null;
+        ArrayList<Integer> temp = new ArrayList<Integer>(arr1);
+        temp.removeAll(arr2);
+        return temp;
     }
 }
