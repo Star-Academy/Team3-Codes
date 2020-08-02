2671c6f880180f7d2bf4be7caceb589c4d702abc
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2020-08-02 10:55:22 +0430

    added arithmetic class

---
+++ Phase01-Search/Arithmetic.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class Arithmetic {
+    
+    public ArrayList<Integer> and(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
+        return null;
+    }
+
+    public ArrayList<Integer> or(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
+        return null;
+    }
+
+    public ArrayList<Integer> subtract(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
+        return null;
+    }
+}
