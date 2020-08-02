9fb9230ed3738b4b2564d1a776a79137a05fc510
mahla <aa@ya.com>
2020-08-02 03:35:34 -0600

    Add method to group the words

--- Phase01-Search/Main.java
+++ Phase01-Search/Main.java
@@ -1,3 +1,5 @@
+import javafx.css.Match;
+
 import java.io.IOException;
 import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
@@ -6,24 +8,31 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class  Main {
     static HashMap<String, ArrayList<Integer>> tokens = new HashMap<>();
+    static ArrayList<String> wordsWithPlusSign = new ArrayList<>();
+    static ArrayList<String> wordsWithMinusSign = new ArrayList<>();
+    static ArrayList<String> noneSignWords = new ArrayList<>();
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String word = scanner.nextLine();
-        fillingTheMap();
-        if(tokens.containsKey(word.toLowerCase()))
-             for (Integer integer : tokens.get(word.toLowerCase())) {
-                 System.out.print(integer + " ");
-              }
-        else
-            System.err.println("The word does not exist !");
+        String input = scanner.nextLine();
+        fillTheMap();
+        assortTheWords(input);
+//        if(tokens.containsKey(word.toLowerCase()))
+//             for (Integer integer : tokens.get(word.toLowerCase())) {
+//                 System.out.print(integer + " ");
+//              }
+//        else
+//            System.err.println("The word does not exist !");
 
 
     }
 
-    public static void fillingTheMap(){
+    public static void fillTheMap(){
         InvertedIndex invertedIndex = new InvertedIndex(tokens);
         FileReader fileReader = new FileReader();
 
@@ -37,5 +46,28 @@ public class  Main {
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public static void assortTheWords(String input){
+        String[] splitInput = input.split(" ");
+        Pattern pattern = Pattern.compile("(\\+|-)(\\w*)");
+
+        for (String s : splitInput) {
+            Matcher matcher = pattern.matcher(s);
+            if(matcher.matches()) {
+                switch (matcher.group(1)) {
+                    case "+":
+                        wordsWithPlusSign.add(matcher.group(2));
+                        break;
+
+                    case "-":
+                        wordsWithMinusSign.add(matcher.group(2));
+                        break;
+                }
+            }
+            else
+                noneSignWords.add(s);
+        }
+
     }
 }
