79ceedd99f45f4fc5ad974ddefa21f7757edf86d
mahla <aa@ya.com>
2020-08-02 14:42:26 -0600

    Delete some lines .

--- Phase01-Search/Arithmetic.java
+++ Phase01-Search/Arithmetic.java
@@ -1,21 +1,16 @@
+
 import java.util.ArrayList;
 
 public class Arithmetic {
 
     public static ArrayList<Integer> and(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
-        if (arr1.size() == 0) {
-            return arr2;
-        }
-        if (arr2.size() == 0) {
-            return arr1;
-        }
-        ArrayList<Integer> temp = new ArrayList<Integer>(arr1);
+        ArrayList<Integer> temp = new ArrayList<>(arr1);
         temp.retainAll(arr2);
         return temp;
     }
 
     public static ArrayList<Integer> or(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
-        ArrayList<Integer> temp = new ArrayList<Integer>(arr1);
+        ArrayList<Integer> temp = new ArrayList<>(arr1);
         for (Integer integer : arr2) {
             if (!temp.contains(integer)) {
                 temp.add(integer);
@@ -24,11 +19,10 @@ public class Arithmetic {
         return temp;
     }
 
-    public static ArrayList<Integer> subtract(ArrayList<Integer> arr1, ArrayList<Integer>... arr2) {
-        ArrayList<Integer> temp = new ArrayList<Integer>(arr1);
-        for (ArrayList<Integer> integers : arr2) {
-            temp.removeAll(integers);
-        }
+    public static ArrayList<Integer> subtract(ArrayList<Integer> arr1, ArrayList<Integer> arr2) {
+        ArrayList<Integer> temp = new ArrayList<>(arr1);
+        temp.removeAll(arr2);
         return temp;
     }
+
 }
