1d01529d864bcfed3b4865fb7809627985ce993e
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2020-08-09 15:14:15 +0430

    fileReaderTest works

--- Search/maven/src/test/java/Search/FileReaderTest.java
+++ Search/maven/src/test/java/Search/FileReaderTest.java
@@ -2,15 +2,32 @@ package Search;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+
 import org.junit.Test;
 
 public class FileReaderTest {
-    private String testPath = "src\\test\\resources\\TestDocs\\doc1.txt";
+    private String testPath = "maven\\src\\test\\resources\\TestDocs";
 
-    // @Test
-    // public void getFileContentsTest() {
-    //     String actualResult = FileReader.getFileContents(testPath);
-    //     String expectedResult = "The sky , at sunset , looked like a carnivorous flower .";
-    //     assertEquals(expectedResult, actualResult);
-    // }
+    @Test
+    public void hasNextTest() {
+        try {
+            FileReader reader = new FileReader(testPath);
+            assertEquals(reader.hasNext(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void NextTest() {
+        try {
+            FileReader reader = new FileReader(testPath);
+            String actual = reader.next();
+            String expected = "The sky , at sunset , looked like a carnivorous flower .";
+            assertEquals(expected, actual);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
