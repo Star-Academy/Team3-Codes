d20f3e6bf162b9f344281681a55f4a4bebf4296f
mahla <aa@ya.com>
2020-08-09 02:05:01 -0600

    Add test of InvertedIndex class

--- Search/maven/src/test/java/Search/InvertedIndexTest.java
+++ Search/maven/src/test/java/Search/InvertedIndexTest.java
@@ -4,40 +4,41 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Scanner;
 
-
 import org.junit.Before;
 
 public class InvertedIndexTest {
-    private static HashMap <String, ArrayList<Integer>> mapOfTheWords ;
-    Scanner scanner ;
+    private static HashMap<String, ArrayList<Integer>> mapOfTheWords;
+    Scanner scanner;
 
     @Before
-    public void initMap(){
+    public void initMap() {
         mapOfTheWords = new HashMap<>();
-        mapOfTheWords.put("sky",new ArrayList<>(Arrays.asList(2)));
-        mapOfTheWords.put("sunset",new ArrayList<>(Arrays.asList(1,3)));
-        mapOfTheWords.put("tree",new ArrayList<>(Arrays.asList(5)));
+        mapOfTheWords.put("sky", new ArrayList<>(Arrays.asList(2)));
+        mapOfTheWords.put("sunset", new ArrayList<>(Arrays.asList(1, 3)));
+        mapOfTheWords.put("tree", new ArrayList<>(Arrays.asList(5)));
     }
-    
+
     @Test
-    public void updateTheMapTest(){
-    
-        scanner = new Scanner("src\\test\\resources\\TestDocs\\doc1.txt");
+    public void updateTheMapTest() {
+
+        scanner = new Scanner("The sky , at sunset , looked like a carnivorous flower .");
+      
       
         InvertedIndex invertedIndex = new InvertedIndex(mapOfTheWords);
-        // invertedIndex.updateTheMap(scanner.nextLine(), 1);
+        invertedIndex.updateTheMap(scanner.nextLine(), 1);
         
-        // assertArrayEquals(, actuals);
-        assertEquals(mapOfTheWords.get("sky").size(), 2);
-        // assertEquals(mapOfTheWords.get("sunset").size(),2);
-        // assertEquals(mapOfTheWords.get("tree").size(),1);
-        assertEquals(mapOfTheWords.size(),3);
-        //  assertEquals(mapOfTheWords.get("flower").size(),1);
+         assertEquals(mapOfTheWords.get("sky").size(), 2);
+         assertEquals(mapOfTheWords.get("sunset").size(),2);
+         assertEquals(mapOfTheWords.get("tree").size(),1);
+    
+         assertEquals(mapOfTheWords.get("flower").size(),1);
         
         assertEquals(1, 1);
         
