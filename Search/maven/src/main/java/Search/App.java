4ac5d60f8de7605a036a90d8d6c285924cc1111a
mahla <aa@ya.com>
2020-08-09 02:05:28 -0600

    new change in Main class

--- Search/maven/src/main/java/Search/App.java
+++ Search/maven/src/main/java/Search/App.java
@@ -8,8 +8,9 @@ public class App {
         Scanner scanner = new Scanner(System.in);
         Processor processor = new Processor();
 
-        System.out.println("Enter path of directory which documents are located .");
-        int numberOfDocs = processor.fillTheMap(scanner.nextLine());
+        // System.out.println("Enter path of directory which documents are located .");
+        // int numberOfDocs = processor.fillTheMap(scanner.nextLine());
+         int numberOfDocs = processor.fillTheMap("maven\\src\\main\\resources\\Docs");
         
         System.out.println("Enter the string of words which you want to search .");
         for (Integer integer : processor.process(scanner.nextLine(), numberOfDocs))
