b2d0100d9b2923fc45d57988dfc4a3a64ea65e8d
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2020-08-12 13:36:16 +0430

    nothing

--- phase4/Program.cs
+++ phase4/Program.cs
@@ -11,14 +11,11 @@ namespace phase4
             var reader = new Reader();
             var grades = reader.Read<Grade>("resources\\Scores.json");
             var studentInfos = reader.Read<StudentInfo>("resources\\Students.json");
-
             var students = studentInfos.Select(s => new Student(s)).ToList();
             students.ForEach(s => s.grades = grades.FindAll(g => g.StudentNumber == s.Info.StudentNumber));
             students = students.OrderByDescending(s => s.GetAverage()).ToList();
-
             foreach( var student in students.Take(3))
                 Console.WriteLine(student);
-        
         }
     }
 }
