c77e06258155faee64ff1fb6a4392dc046d98456
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2020-08-15 11:57:49 +0430

    Reader and FileReaderTest totally work

---
+++ Phase05/Search/Search.Test/UtilsTest/FileReaderTest.cs
@@ -0,0 +1,28 @@
+using Xunit;
+using Search.Utils;
+
+namespace Search.Test.UtilsTest
+{
+    public class FileReaderTest
+    {
+        private const string TestPath =  "resources";
+
+        [Fact]
+        public void MoveNextTest()
+        {
+            FileReader reader = new FileReader(TestPath);
+            Assert.Equal(reader.MoveNext(),true);
+        }
+
+        [Fact]
+        public void CurrentTest()
+        {
+            FileReader reader = new FileReader(TestPath);
+            reader.MoveNext();
+            string actual = reader.Current();
+            string expected = "The sky , at sunset , looked like a carnivorous flower .";
+            Assert.Equal(actual,expected);
+        }
+        
+    }
+}
