565e6a8d36a7b133d110c9415fabfe2b259e307a
mahla <aa@ya.com>
2020-08-17 11:51:51 -0600

    Add console app that works with library

---
+++ phase6/ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using Search.Model;
+using Search.Utils;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        private const string path = "Docs";
+        static void Main(string[] args)
+        {
+
+            // Console.WriteLine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+            var reader = new FileReader(path);
+            var processor  = new Processor(reader);
+            var numberOfDocs = processor.FillTheMap();
+
+            Console.WriteLine("Enter the string of words which you want to search .");
+
+            foreach (var integer in processor.Process(Console.ReadLine(), numberOfDocs))
+                Console.Write($"{integer} ");
+        }
+    }
+}
