5711e94d2b3f9202c219515f58d58f0997719c5a
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2020-08-19 16:47:43 +0430

    nothing

--- phase08/NestTest/Program.cs
+++ phase08/NestTest/Program.cs
@@ -14,6 +14,7 @@ namespace NestTest
             // and should not be used in production
             connectionSettings.EnableDebugMode();
             var client = new ElasticClient (connectionSettings);
+            var response = client.Ping();
         }
     }
 }
