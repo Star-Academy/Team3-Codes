23cbca4f02a83234e54b340b68f47043c079d583
mahla <aa@ya.com>
2020-08-19 06:48:58 -0600

    make project excutable

--- phase08/NestTest/Program.cs
+++ phase08/NestTest/Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
-using Nest;
+
 namespace NestTest
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var uri = new Uri ("http://localhost:9200");
-            var connectionSettings = new ConnectionSettings (uri);
-            // DebugMode gives you the request in each request to make debuging easier
-            // But don't forget to only use it in debugging, because its usage has some overhead
-            // and should not be used in production
-            connectionSettings.EnableDebugMode();
-            var client = new ElasticClient (connectionSettings);
-            var response = client.Ping();
+            Console.WriteLine("Hello World!");
         }
     }
 }
