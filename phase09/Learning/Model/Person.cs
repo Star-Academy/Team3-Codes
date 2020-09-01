3ee75dd37cbccdac81b77982f067cf89fc4ff336
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2020-09-01 11:18:56 +0430

    some changes addes

---
+++ phase09/Learning/Model/Person.cs
@@ -0,0 +1,10 @@
+namespace Learning.Model
+{
+    public class Person 
+    {
+        public string Name{get;set;}
+        public string LastName{get;set;}
+
+        public int Id{get;set;}
+    }
+}
