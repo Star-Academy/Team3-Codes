663059018304a6874229be83518b4bc19b8255f8
kimianoorbakhsh <kimianoorbakhsh@gmail.com>
2020-09-06 10:38:52 +0430

    added a javascript file

---
+++ phase09/Project/FrontEnd/scripts.js
@@ -0,0 +1,20 @@
+function checkForEnter(event) {
+    if (event.key === 'Enter') {
+        const input = document.querySelector('.search-input');
+        const value = input.value;
+        const request = {
+            city: value
+        };
+
+        const xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function () {
+            if (this.readyState === 4 && this.status === 200) {
+                //
+            }
+        };
+        xhttp.open('POST', 'https://localhost:5001/Search/GetMatchedResult');
+        xhttp.setRequestHeader('Content-type', 'application/json');
+        xhttp.responseType = 'json';
+        xhttp.send(JSON.stringify(request));
+    }
+}
