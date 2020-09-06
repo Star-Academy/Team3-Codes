88ed9c2364951c5a8579899811a654b5fcfc6267
mahla <aa@ya.com>
2020-09-06 12:16:30 -0600

    Add first style of showing results

--- phase09/Project/FrontEnd/scripts.js
+++ phase09/Project/FrontEnd/scripts.js
@@ -2,11 +2,14 @@ function checkForEnter(event) {
     if (event.key === 'Enter') {
         const input = document.querySelector('.search-input');
         const value = input.value;
+        const request = {
+            input: value
+        };
 
         const xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (this.readyState === 4 && this.status === 200) {
-                console.log(this.response);
+                displayDocs(this.response);
             }
         };
         xhttp.open('POST', 'https://localhost:5001/Search/GetMatchedResult');
@@ -15,3 +18,40 @@ function checkForEnter(event) {
         xhttp.send(JSON.stringify(value));
     }
 }
+
+function displayDocs(docs) {
+    let template = '';
+
+    let counter = 1;
+    for (const doc of docs) {
+        template += 
+                `<button type="button" class="collapsible">${doc.name}</button>
+                <div class="content">
+                 <p>
+                    ${doc.text}
+                </p>
+                </div>
+               
+                <b>${counter}</b>`;
+        counter++;
+    }
+
+    document.getElementById('whole-body').innerHTML = template;
+    var coll = document.getElementsByClassName("collapsible");
+    var i;
+
+    for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            var content = this.nextElementSibling;
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    }
+}
+
+
+
