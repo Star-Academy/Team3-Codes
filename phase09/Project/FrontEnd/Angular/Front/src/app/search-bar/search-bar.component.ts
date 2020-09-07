20656dc1e2684eeca728080bdd4fd114a2705cd6
mahla <aa@ya.com>
2020-09-07 14:02:11 -0600

    make two-way-binding for input of search bar

--- phase09/Project/FrontEnd/Angular/Front/src/app/search-bar/search-bar.component.ts
+++ phase09/Project/FrontEnd/Angular/Front/src/app/search-bar/search-bar.component.ts
@@ -9,20 +9,23 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./search-bar.component.scss']
 })
 export class SearchBarComponent implements OnInit {
-  @Output()
-  public searched = new EventEmitter<string>();
-  public value :'';
-  // constructor(private router: Router) {}
+
+    @Output()
+    public searched = new EventEmitter<string>();
   
-  // this.router.navigate([ 'search-results', query ]);
-  ngOnInit(): void {
-  }
-
-  public checkForEnter(event: KeyboardEvent) {
-    if (event.key === 'Enter') {
-      this.searched.emit(this.value);
+    public value = '';
+  
+    constructor() { }
+  
+    ngOnInit(): void {
     }
+  
+    public checkForEnter(event: KeyboardEvent) {
+      if (event.key === 'Enter') {
+        console.log(this.value);
+        this.searched.emit(this.value);
+      }
+    }
+  
   }
-}
-
 
